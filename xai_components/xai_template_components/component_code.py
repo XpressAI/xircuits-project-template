c5cfe0d11c2fcd75766cc6871f622b9b3599a5e1
MFA-X-AI <68586800+MFA-X-AI@users.noreply.github.com>
2022-05-12 13:53:37 +0700

    added validation automation + example component and xircuits

--- xai_components/xai_template_components/component_code.py
+++ xai_components/xai_template_components/component_code.py
@@ -3,15 +3,19 @@ from xai_components.base import InArg, OutArg, InCompArg, Component, xai_compone
 @xai_component(color="red")
 class HelloNewLibrary(Component):
 
+    greet_str: InArg[str]
+
+
     def __init__(self):
 
         self.done = False
+        self.greet_str = InArg.empty()
 
     def execute(self, ctx) -> None:
-        
+
         import os 
 
-        creator_name = os.getlogin()
-        print("Hello, " + creator_name)
+        greet_str = self.greet_str.value if self.greet_str.value else "Xircuits Project!"
+        print("Hello, " + greet_str)
 
         self.done = True
